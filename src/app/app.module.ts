956dfe6b0771b1613d3b08acfd1c3996dc327fec
Ralph Tamayo <ralphtamayo@creatinginfo.com>
2020-09-01 21:51:05 +0800

    Add a simple 'Under Construction' page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnderConstructionPage } from './under-construction/under-construction.page';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: "under-construction", component: UnderConstructionPage },
+  { path: "**", redirectTo: "under-construction" }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnderConstructionPage
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/under-construction/under-construction.page.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'under-construction-page',
+  templateUrl: './under-construction.page.html'
+})
+export class UnderConstructionPage {
+}
