51baafc0f08c17f3598961a7462131c8336a25cb
Ralph Tamayo <ralphtamayo@creatinginfo.com>
2020-09-01 21:58:31 +0800

    Add page title and delete favicon

--- src/app/under-construction/under-construction.page.ts
+++ src/app/under-construction/under-construction.page.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'under-construction-page',
   templateUrl: './under-construction.page.html'
 })
-export class UnderConstructionPage {
+export class UnderConstructionPage implements OnInit {
+  public constructor(private titleService: Title ) { }
+
+  ngOnInit() {
+    this.titleService.setTitle('RT | Under Construction')
+  }
 }
